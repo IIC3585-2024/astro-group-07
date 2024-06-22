4a04a21c5e3e8a1e1d0037d8a47949a9c9e674b3
Pedro Palma <pedro.palma@uc.cl>
2024-06-21 21:29:06 -0400

    feat: Connect app to Movies API

---
+++ src/lib/getSeriesByName.js
@@ -0,0 +1,13 @@
+export default async function getMoviesByName(name) {
+  const token = import.meta.env.TMDB_TOKEN
+  const options = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+      }
+  }
+  const response = await fetch(`https://api.themoviedb.org/3/search/tv?query=${name}&language=en-US`, options)
+  const data = await response.json()
+  return data.results
+}
---
+++ src/lib/getSeriesInfo.js
@@ -0,0 +1,13 @@
+export default async function getSeriesInfo(id) {
+  const token = import.meta.env.TMDB_TOKEN
+  const options = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+      }
+  }
+  const response = await fetch(`https://api.themoviedb.org/3/tv/${id}?language=en-US`, options)
+  const data = await response.json()
+  return data
+}
