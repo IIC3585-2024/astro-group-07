15916850f8d1899dbb15664c530f7fe8fb3b383d
Pedro Palma <pedro.palma@uc.cl>
2024-06-23 01:01:16 -0400

    feat: Create Review related components

---
+++ src/components/Review.svelte
@@ -0,0 +1,15 @@
+<script>
+    export let review
+</script>
+
+<li>
+    <h3>{review.username}</h3>
+    <p>{review.rating}</p>
+    <p>{review.content}</p>
+</li>
+
+<style>
+    li {
+        list-style-type: none;
+    }
+</style>
---
+++ src/components/ReviewsList.svelte
@@ -0,0 +1,22 @@
+<script>
+    import { onMount } from 'svelte';
+    import Review from './Review.svelte';
+
+    export let serieId
+
+    let reviews = []
+
+    onMount(() => {
+        console.log(serieId)
+        // TODO: fetch reviews for the serieId
+    })
+</script>
+
+<ul>
+    {#each reviews as review, index}
+        <Review 
+            review={review}
+            key={index}
+        />
+    {/each}
+</ul>
