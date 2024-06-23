9c9f6f943bc33ec465f7af47bd3e23b65038a4da
Pedro Palma <pedro.palma@uc.cl>
2024-06-23 14:51:42 -0400

    feat: Add ReviewSearch

---
+++ src/components/ReviewSearch.svelte
@@ -0,0 +1,50 @@
+<script>
+    import SeriesCard from './SeriesCard.svelte';
+    import getSeriesByName from '../lib/getSeriesByName.js';
+
+    let search = '';
+    let results = [];
+    let searchBy = 'series';
+
+    function searchReviews() {
+        const getFunction = getFunctionBySearchBy();
+        getFunction(search).then((data) => {
+            results = data
+        }).catch((error) => {
+            console.log(error)
+        })
+    }
+
+    function getFunctionBySearchBy() {
+        switch (searchBy) {
+            case 'series':
+                return getSeriesByName;
+            default:
+                return getSeriesByName;
+        }
+    }
+</script>
+
+<div>
+    <input type="text" bind:value={search} placeholder="Search for a series" />
+    <button on:click={searchReviews}>Search</button>
+    <ul>
+        {#each results as result}
+            {#if searchBy === 'series'}
+                <SeriesCard
+                    id={result.id}
+                    name={result.name}
+                    year={result.first_air_date}
+                    posterPath={result.poster_path}
+                />
+            {:else if searchBy === 'review'}
+                <li>
+                    <a href={`/review/${result.id}`}>
+                        <h3>{result.title}</h3>
+                        <p>{result.content}</p>
+                    </a>
+                </li>
+            {/if}
+        {/each}
+    </ul>
+</div>
---
+++ src/components/SeriesCard.svelte
@@ -0,0 +1,33 @@
+<script>
+    export let name;
+    export let year;
+    export let posterPath;
+    export let id;
+
+    $: if (year) {
+        year = year.split('-')[0];
+    }
+</script>
+
+<li>
+    <a href={`/series/${id}`}>
+        <img src={`https://image.tmdb.org/t/p/w500${posterPath}`} alt={name} />
+        <h3>{name}</h3>
+        <p>{year}</p>
+    </a>
+</li>
+
+<style>
+    li {
+        list-style: none;
+    }
+    
+    a {
+        text-decoration: none;
+        color: black;
+    }
+    a:hover {
+        text-decoration: underline;
+    }
+
+</style>
