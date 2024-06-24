b093a68914d3754b12fb83632afb7dfc831817fe
Pedro Palma <pedro.palma@uc.cl>
2024-06-23 23:30:29 -0400

    feat: Change input type when changing searchBy

--- src/components/ReviewSearch.svelte
+++ src/components/ReviewSearch.svelte
@@ -1,13 +1,15 @@
 <script>
+    import { onMount } from 'svelte';
     import SeriesCard from './SeriesCard.svelte';
     import { search } from '../lib/db/search.js';
-    import Review from './Review.svelte';
+    import getGenres from '../lib/getGenres.js';
 
     let searchText = '';
     let results = [];
     let searchBy = 'title';
     let rating = '';
-    let order = 'ascending';
+    let order = 'asc';
+    let genres = [];
 
     function searchReviews() {
         search(searchText, searchBy, rating, order).then((data) => {
@@ -17,18 +19,36 @@
             console.error(error);
         });
     }
+
+    onMount(async () => {
+        genres = await getGenres();
+    });
 </script>
 
 <div>
-    <input type="text" bind:value={searchText} placeholder={`Search for a ${searchBy}`} />
-    <select bind:value={searchBy}>
+    {#if searchBy === 'title'}
+        <input type="text" bind:value={searchText} placeholder={`Search for a ${searchBy}`} />
+    {:else if searchBy === 'genre'}
+        <select bind:value={searchText}>
+            <option value="" disabled selected>Select a genre</option>
+            {#each genres as genre}
+                <option value={genre.name}>{genre.name}</option>
+            {/each}
+        </select>
+    {:else if searchBy === 'service'}
+        <select bind:value={searchText}>
+            <!-- TODO: Add services -->
+            <option value="netflix">Netflix</option>
+        </select>
+    {/if}
+    <select bind:value={searchBy} on:input={() => searchText = ''}>
         <option value="title">Title</option>
         <option value="genre">Genre</option>
         <option value="service">Service</option>
     </select>
     <input type="number" bind:value={rating} min="1" max="10" placeholder="Rating" />
     <select bind:value={order}>
-        <option value="asc">Ascending</option>
+        <option value="asc" selected>Ascending</option>
         <option value="desc">Descending</option>
     </select>
     <button on:click={searchReviews}>Search</button>
